8fa16f2a0c5aef743e4bf0ffb8c22f92d2482eea
YogasWara18 <teukukurniayogaswara@gmail.com>
2025-05-12 22:05:31 +0700

    learn

---
+++ module-3-tipe-data/script.js
@@ -0,0 +1,11 @@
+// Tipe Data STRING 
+const nama = 'Ranggo'
+const negara = "Indonesia"
+const domisili = `${nama} Bandung` // ${nama} Cara untuk mengambil variabel tertentu.
+
+console.log(nama,typeof nama) // 'typeof' berguna untuk mengecek tipe data
+
+// Tipe Data NUMBER 
+const umur = 26
+
+console.log(umur,typeof umur)
