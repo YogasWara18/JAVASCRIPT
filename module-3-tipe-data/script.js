f90995df4c5fd738330ab78206fb23ecb2e81a1e
YogasWara18 <teukukurniayogaswara@gmail.com>
2025-05-13 14:15:41 +0700

    learn

--- module-3-tipe-data/script.js
+++ module-3-tipe-data/script.js
@@ -7,5 +7,18 @@ console.log(nama,typeof nama) // 'typeof' berguna untuk mengecek tipe data
 
 // Tipe Data NUMBER 
 const umur = 26
+// const umur = 26.5 tipe data FLOAT NUMBER
 
 console.log(umur,typeof umur)
+
+// Tipe Data BOOLEAN 
+const isMarried = true 
+const notMarried = false 
+
+console.log(isMarried, typeof isMarried)
+console.log(notMarried, typeof notMarried)
+
+// Undefined 
+let x;   // variabel x undefined karna belum di defenisikan.
+
+console.log(x, typeof x)
