02adb117f9f3b571aa476b0b544110aaa624956a
YogasWara18 <teukukurniayogaswara@gmail.com>
2025-05-24 19:54:47 +0700

    Update

---
+++ module-5-operator-aritmatika/script.js
@@ -0,0 +1,36 @@
+/* OPERATOR ARITMATIKA */
+
+// + ( Penjumlahan )
+let x = 5 
+let y = 10 
+let sum = x + y         // Operator penjumlahan ( + ) untuk kita bisa menambahkan sesuatu 
+
+console.log(sum, typeof sum )  // Output: 15 number, Tipe data mengikuti dari value ( nilai ) dari variabel.
+
+// - ( Pengurangan )
+let a = 45
+let b = 32
+let diff = a - b        // Operator pengurangan ( - ) dilakukan untuk mengurangi sesuatu 
+
+console.log(diff, typeof diff)  // Output: 13 number, Tipe data mengikuti dari value ( nilai ) dari variabel.
+
+// * ( Perkalian )
+let p = 5
+let q = 8
+let result = p * q      // Operator perkalian ( * ) dilakukan untuk mengkali sesuatu 
+
+console.log(result, typeof result)  // Output: 40 number, Tipe data mengikuti dari value ( nilai ) dari variabel.
+
+// / ( Pembagian )
+let g = 100
+let s = 5 
+let bagi = g / s        // Operator pembagian ( / ) dilakukan untuk membagi sesuatu
+
+console.log(bagi, typeof bagi)  // Output : 20 number, Tipe data mengikuti dari value ( nilai ) dari variabel.
+
+// % ( Modulus: Sisa bagi )
+let t = 23
+let r = 3 
+let modulus = t % r         // Operator Modulus:sisa bagi ( % ) dilakukan pada
+
+console.log(modulus, typeof modulus)
