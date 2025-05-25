c8de42efe2efd9ca7371df4aac3eafad381f6a6e
YogasWara18 <teukukurniayogaswara@gmail.com>
2025-05-25 22:04:14 +0700

    Update

--- module-5-operator/scriptAritmatika.js
+++ module-5-operator/scriptAritmatika.js
@@ -31,6 +31,25 @@ console.log(bagi, typeof bagi)  // Output : 20 number, Tipe data mengikuti dari 
 // % ( Modulus: Sisa bagi )
 let t = 23
 let r = 3 
-let modulus = t % r         // Operator Modulus:sisa bagi ( % ) dilakukan menghitung sisa bagi 
+let modulus = t % r         // Operator Modulus:sisa bagi ( % ) dilakukan menghitung sisa bagi dengan menghitung nilai terdekat dari nilai yang dibagi.
 
 console.log(modulus, typeof modulus) // Output: 2 number, Tipe data mengikuti dari value ( nilai ) dari variabel.
+
+// ** ( Eksponen )
+let base = 2
+let exponent = 6
+let power = base ** exponent        // Operator Eksponen untuk bilangan nilai yang dipangkatkan, Jumlah hasil dari operator eksponen ini adalah jumlah yang sudah di hitung pangkatnya 
+
+console.log(power, typeof power)   // Output: 64 number, Tipe data mengikuti value ( nilai ) dari variabel.
+
+// ++ ( Increment = Ditambah 1 )
+let count = 9
+count++             // Operator Increment = ditambah satu ( 1 ), Setiap nilai dari variabel akan ditambah dengan 1 jika memakai operator Increment.
+
+console.log(count, typeof count)  // Output: 10 number
+
+// -- ( Decrement = Dikurang 1 )
+let decremen = 99
+decremen--          // Operator Decrement = Dikurang satu ( 1 ), setiap nilai dari variabel akan dikurang dengan 1 jika memekai operator Decrement.
+
+console.log(decremen, typeof decremen) // Output: 98 number
---
+++ module-5-operator/scriptAssignment.js
@@ -0,0 +1,8 @@
+/* OPERATOR ASSIGNMENT */
+
+// = ( Assignment dasar )
+let nama = "Ranggo"   // Contoh Assignment dasar ( = ) dan memberikan nilai ke variabel "Ranggo".
+
+console.log(nama, typeof nama)   // Output: Ranggo string.
+
+// += ( Assignment Penjumlahan )
