9d30aa1383a009bebf357ab36815b0680c8f5acb
YogasWara18 <teukukurniayogaswara@gmail.com>
2025-05-26 11:01:34 +0700

    Update

--- module-5-operator/scriptAssignment.js
+++ module-5-operator/scriptAssignment.js
@@ -6,3 +6,25 @@ let nama = "Ranggo"   // Contoh Assignment dasar ( = ) dan memberikan nilai ke v
 console.log(nama, typeof nama)   // Output: Ranggo string.
 
 // += ( Assignment Penjumlahan )
+let x = 7           // Mendeklarasikan value ( nilai  ) x = 7 
+x += 5              // += ( Operator ), value ( nilai ) x = 7 ditambah ( += ) dengan value ( nilai ) 5 menjadi = 12
+
+console.log(x, typeof x)  // Output: 12 Number.
+
+// -= ( Assignment Pengurangan )
+let y = 9           // Mendeklarasikan value ( nilai ) y = 9
+y -= 5              // -= ( Operator ), value ( nilai ) y = 9 dikurang ( -= ) dengan value ( nilai ) 5 menjadi = 4
+
+console.log(y, typeof y)  // Output: 4 Number.
+
+// *= ( Assignment Perkalian )
+let k = 12          // Mendeklarasikan value ( nilai ) k = 12
+k *= 4              // *= ( Operator ),value ( nilai ) k = 12 dikali ( *= ) dengan value ( nilai ) 4 menjadi = 48
+
+console.log(k, typeof k)  // Output: 48 Number.
+
+// /= ( Assignment Pembagian )
+let b = 20          // Mendeklarasikan value ( nilai ) b = 20 
+b /= 2              // /= ( Operator ), value ( nilai ) b = 20 dibagi ( /= ) dengan value ( nilai ) 2 menjadi = 10 
+
+console.log(b, typeof b)  // Ouput: 10 Number.
