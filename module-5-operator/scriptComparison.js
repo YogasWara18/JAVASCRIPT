05e86f512a902cb2a350806536cac399deebab53
YogasWara18 <teukukurniayogaswara@gmail.com>
2025-05-26 16:46:43 +0700

    Update

--- module-5-operator/scriptComparison.js
+++ module-5-operator/scriptComparison.js
@@ -1,3 +1,20 @@
 /* OPERATOR COMPARISON */
+// OPERATOR COMPARISON berlaku dan digunakan pada console.log
 
 // == ( Mengecek atau membandingkan persamaan nilai )
+let a = "5"     // String "5"
+let b = 5       // Number 5 
+
+console.log(a == b) // Output: True ( Boolean ), Karna Value ( nilai ) dari kedua variabel a & b itu sama.
+/* Operator Comparison == adalah operator yang digunakan untuk membandingkan dua nilai atau ekspresi untuk menentukan apakah mereka sama. Jika kedua nilai yang dibandingkan memiliki nilai yang sama, maka hasil dari operasi ini adalah TRUE. Jika tidak, hasilnya adalah FALSE.*/
+/* Operator == biasanya bekerja dengan membandingkan nilai dari kedua operand tanpa memperhatikan tipe data.*/
+
+// === ( Mengecek atau membandingkan persamaan nilai dan tipe data )
+let c = "10"    // String "10"
+let d = 10      // Number 10
+
+console.log(c === d) // Output: False ( Boolean ), Karna tipe data dari value ( nilai ) dari kedua variabel berbeda.
+/* Operator === dalam javaScript dikenal sebagai STRICT EQUALITY OPERATOR ini membandingkan nilai dan tipe data dari dua operand, dan hanya akan mengembalikan TRUE jika kedua operand ( variabel ) memiliki nilai yang sama dan tipe data yang sama. Jika ada perbedaan dalam tipe data atau nilai, maka hasilnya adalah FALSE. */
+
+
+
